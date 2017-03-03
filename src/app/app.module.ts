e75079e30f676d1fd18281ed09c3a104c382f516
Alejandra Cardenas <ale_sorpresa28@hotmail.com>
2017-03-03 04:58:02 -0700

    antes de modificar chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { O2chartComponent } from './o2chart/o2chart.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+    HttpModule
+     ],
+  declarations: [ 
+    AppComponent,
+    O2chartComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,32 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import {NgModule, ModuleWithProviders} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {O2ChartComponent} from './app/o2chart/o2chart.component';
+import {ChartConst} from './app/o2chart/shared/chart-const';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+export * from './app/o2chart/o2chart.component';
+export * from './app/o2chart/shared/chart-const';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ChartConst,
+    O2ChartComponent
+  ],
+  exports: [
+    O2ChartComponent
+  ]
+})
+export default class O2ChartModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: O2ChartModule,
+      providers: []
+    };
+  }
+}
